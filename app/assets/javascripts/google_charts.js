7a6aaf27bf9b27226e9c6041c326e171aa358058
Max DePriest <maxd924@gmail.com>
2015-06-29 23:53:50 -0500

    Can't get the javascript to work without refreshing the page

--- app/assets/javascripts/google_charts.js
+++ app/assets/javascripts/google_charts.js
@@ -29,52 +29,56 @@ function drawChart(e1, e2, e3, e4, e5, e6, results) {
 }
 
 function getDataPoints () {
-  if (window.location.pathname == "/back_results" || window.location.pathname == "/back_results/show") {
-    type = "back_results";
-  } else if (window.location.pathname == "/chest_results")  {
-    type = "chest_results"
-      
-  } else if (window.location.pathname == "/legs_results")  {
-    type = "legs_results";
-  }
-  
-  $.ajax( {
-    url: "/" + type,
-    type: "get",
-    dataType: "json",
+  $(document).ready(function() {
 
-    success: function(data)  {
-      var results = []; 
-      var counter = 1;   
-      for (var i = 0; i < data.length; i++) {
-        var array = $.map(data[i], function(value, index) {
-          return [value];
-        });
 
-        var userTest = parseInt($('#userId').val());
-        console.log(userTest);
-        if (array[(array.length - 1)] == userTest)  {
-          array.splice(-1,1);
-          array[0] = (counter);
-          results.push(array);
-          counter++;
+    if (window.location.pathname == "/back_results" || window.location.pathname == "/back_results/show") {
+      type = "back_results";
+    } else if (window.location.pathname == "/chest_results")  {
+      type = "chest_results"
+        
+    } else if (window.location.pathname == "/legs_results")  {
+      type = "legs_results";
+    }
+    
+    $.ajax( {
+      url: "/" + type,
+      type: "get",
+      dataType: "json",
+
+      success: function(data)  {
+        var results = []; 
+        var counter = 1;   
+        for (var i = 0; i < data.length; i++) {
+          var array = $.map(data[i], function(value, index) {
+            return [value];
+          });
+
+          var userTest = parseInt($('#userId').val());
+          console.log(userTest);
+          if (array[(array.length - 1)] == userTest)  {
+            array.splice(-1,1);
+            array[0] = (counter);
+            results.push(array);
+            counter++;
+          }
+
         }
 
-      }
+        if (type == "back_results") {
+          drawChart("Seated Row", "Lat Pulldown", "Renegade Row", "Seated Good Mornings", "Deadlift", "Deltoid Fly", results);
+        } else if (type == "chest_results")  {
+          drawChart("Flat Bench Press", "Incline Dumbbell Press", "Cable Fly", "Decline Bench Press", "Incline Fly", "Stabilization Pushup", results);
+        
+        } else if (type == "legs_results")  {
+          drawChart("Back Squat", "Calf Raises", "Leg Press", "Leg Curl", "Leg Extension", "Lunges", results);
+        }
+            
 
-      if (type == "back_results") {
-        drawChart("Seated Row", "Lat Pulldown", "Renegade Row", "Seated Good Mornings", "Deadlift", "Deltoid Fly", results);
-      } else if (type == "chest_results")  {
-        drawChart("Flat Bench Press", "Incline Dumbbell Press", "Cable Fly", "Decline Bench Press", "Incline Fly", "Stabilization Pushup", results);
-      
-      } else if (type == "legs_results")  {
-        drawChart("Back Squat", "Calf Raises", "Leg Press", "Leg Curl", "Leg Extension", "Lunges", results);
+      },
+      failure: function() {
+        console.log("No dice");
       }
-          
-
-    },
-    failure: function() {
-      console.log("No dice");
-    }
+    });
   });
 }
